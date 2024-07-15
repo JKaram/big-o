935717d310abd76c2c2f0c2eb0f1ed7ecaa6a85a
Jamie Karam <x-25@Jamies-MacBook-Pro.local>
2024-07-14 22:03:41 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import BarChart from "./components/Charts/BarChart";
+
+function App() {
+  return <BarChart width={450} height={300} />;
+}
+
+export default App;
---
+++ src/components/charts/BarChart.tsx
@@ -0,0 +1,82 @@
+import * as d3 from "d3";
+import { useEffect, useState } from "react";
+const barWidth = 60;
+
+const BarChart = ({ width, height }) => {
+  const [data, setData] = useState([200, 170, 140, 250, 160, 130]);
+
+  useEffect(() => {
+    bubbleSort([...data]); // create a copy of data to avoid mutating the original array
+  }, []);
+
+  const bubbleSort = async (arr) => {
+    let swapped;
+
+    do {
+      swapped = false;
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+
+          const current = `#rect${arr[i]}`;
+          const right = `#rect${arr[i + 1]}`;
+
+          const currentX = d3.select(current).attr("x");
+          const rightX = d3.select(right).attr("x");
+
+          d3.select(current).attr("fill", "red");
+          d3.select(right).attr("fill", "yellow");
+
+          await Promise.all([
+            d3
+              .select(`#rect${arr[i]}`)
+              .transition()
+              .duration(1000)
+              .attr("x", rightX)
+              .end(),
+            d3
+              .select(`#rect${arr[i + 1]}`)
+              .transition()
+              .duration(1000)
+              .attr("x", currentX)
+              .end(),
+          ]);
+
+          d3.select(current).attr("fill", "#6baed6");
+          d3.select(right).attr("fill", "#6baed6");
+        }
+      }
+    } while (swapped);
+    return arr;
+  };
+
+  return (
+    <div className="container">
+      <svg
+        className="viz"
+        width={width}
+        height={height}
+        viewBox={`0 0 ${width} ${height}`}
+      >
+        <g className="bars">
+          {data.map((d, i) => (
+            <rect
+              id={`rect${d}`}
+              fill="#6baed6"
+              height={d}
+              key={i}
+              width={barWidth}
+              x={i * (barWidth + 5)}
+              y={height - d}
+            />
+          ))}
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default BarChart;
